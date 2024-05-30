7de78e2fb6927a5528c662fbf647ce14f194d0c0
Naveen <naveennandhi143@gmail.com>
2024-05-30 13:06:44 +0530

    microservice with jwt security

---
+++ src/test/java/com/_techsoft/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com._techsoft.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
